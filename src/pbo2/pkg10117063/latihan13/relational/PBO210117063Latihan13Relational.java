450b372aa700d75892e598718d8fc880278402c8
Cessario <43488314+Cessario@users.noreply.github.com>
2018-09-22 20:41:39 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117063/latihan13/relational/PBO210117063Latihan13Relational.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117063.latihan13.relational;
+
+/**
+ *
+ * @author DEADPOOL
+ *NAMA : Cessario
+ *NIM : 10117063
+ *KELAS : IF-2
+ *Deskripsi Program : menampilkan relasi bilangan*/
+public class PBO210117063Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("c <= a = " + (bil2 <= bil1));
+    }
+    
+}
